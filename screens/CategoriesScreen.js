d30a4ee8cd17ea93b343d810b14706ecd3476aed
gustavo <gustavo@gustavos-iMac.local>
2020-04-22 02:03:07 -0300

    new styles and navigation minor changes

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,20 +3,20 @@ import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 
 import {CATEGORIES} from '../data/dummy-data';
 
+import CategoryGridTitle from '../components/CategoryGridTitle/CategoryGridTitle';
+
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() =>
+      <CategoryGridTitle
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate('CategoryMeals', {
             categoryId: itemData.item.id,
-          })
-        }>
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+          });
+        }}
+      />
     );
   };
 
@@ -30,11 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
   },
 });
 
